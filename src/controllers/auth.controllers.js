a3b90082d7ae792c9767b48c58767e027394156b
Yaswant <yaswantsoni2005@gmail.com>
2026-02-11 13:52:40 +0530

    Auth routes , auth controllers , user model added

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,7 +1,7 @@
 import User from "../models/user.models.js";
 import ApiError from "../utils/ApiError.js";
 import asyncHandler from "../utils/asyncHandler.js";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import { sendRegistrationEmail } from "../services/email.service.js";
 
 
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,6 +3,11 @@ import authController from "../controllers/auth.controllers.js";
 
 const router = express.Router();
 
+/**
+ * - Auth routes
+ * - POST /api/v1/auth/register
+ * - POST /api/v1/auth/login
+ */
 router.post("/register", authController.registerUser);
 router.post("/login", authController.loginUser);
 
