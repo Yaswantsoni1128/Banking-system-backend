4ff760ff535bdf8bef0c089c7ab44c07cab36cdb
Yaswant <yaswantsoni2005@gmail.com>
2026-02-11 13:53:23 +0530

    Auth middleware added

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -1,7 +1,7 @@
 import User from "../models/user.models.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
-import ApiError from "../utils/ApiError.js"
+import ApiError from "../utils/ApiError.js";
 
 const authMiddleware = asyncHandler(async(req , res , next) => {
     const token = req.cookies?.token || req.headers.authorization?.split(" ")[1]
@@ -12,7 +12,7 @@ const authMiddleware = asyncHandler(async(req , res , next) => {
 
     const decoded = jwt.verify(token , process.env.JWT_SECRET);
     console.log(decoded)
-    const user = await User.findOne({ _id: decoded.userId }) // make sure to use userId to find the user
+    const user = await User.findOne({ _id: decoded.userId }) // make sure to use userId
 
     if(!user){
       return next(new ApiError(401, "Unauthorized"))
