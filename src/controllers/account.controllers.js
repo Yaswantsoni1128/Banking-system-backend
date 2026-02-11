53ec0c743a552295077ea172cf14eaae8386f785
Yaswant <yaswantsoni2005@gmail.com>
2026-02-12 03:13:17 +0530

    get user balance and get user account controller added

--- src/controllers/account.controllers.js
+++ src/controllers/account.controllers.js
@@ -1,5 +1,6 @@
 import Account from "../models/account.models.js";
 import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js";
 
 const accountController = {
   /**
@@ -17,6 +18,43 @@ const accountController = {
           data: account
       })
   }),
+  /**
+   * - Get all accounts of the logged in user
+   */
+  getUserAccounts: asyncHandler(async(req, res) => {
+      const user = req.user;
+
+      const accounts = await Account.find({user: user._id});
+
+      return res.status(200).json({
+        success: true,
+        message: "Accounts fetched successfully",
+        data: accounts
+    })
+  }),
+  /**
+   * - Get balance of a specific account
+   */
+  getUserBalance: asyncHandler(async(req, res) => {
+      const {accountId} = req.params;
+
+      const account = await Account.findOne({_id: accountId, user: req.user._id}); 
+
+      if(!account){
+        throw new ApiError(404, "Account not found");
+      }
+
+      const balance = await account.getBalance();
+
+      return res.status(200).json({
+        success: true,
+        message: "Balance fetched successfully",
+        data: {
+          accountId,
+          balance
+        }
+      })
+  })
 };
 
 export default accountController;
